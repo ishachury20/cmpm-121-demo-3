5d46d5074acb86e6efb595c4021c7631f4f8c90a
ishachury20 <35016233+ishachury20@users.noreply.github.com>
2024-11-06 01:58:34 -0800

    added button and alert statement for testing

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 // todo
+
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const gameName = "Blossom Quest";
+document.title = gameName;
+
+const button = document.createElement("button");
+button.innerHTML = `Working?`;
+button.style.color = "#FFFFFF";
+
+button.onclick = () => {
+  alert(`This works properly! :D`);
+};
+
+app.append(button);
